69b786cc4cc6be6d933b1787ffb6f9c0c93edbed
jhyang2020 <yeshenxiaoyu@163.com>
2022-07-10 18:08:56 +0800

    add  minio   --jhyang

---
+++ try-minio/src/main/java/com/fh/tryminio/TryMinioApplication.java
@@ -0,0 +1,13 @@
+package com.fh.tryminio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TryMinioApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TryMinioApplication.class, args);
+    }
+
+}
---
+++ try-minio/src/test/java/com/fh/tryminio/TryMinioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fh.tryminio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TryMinioApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
